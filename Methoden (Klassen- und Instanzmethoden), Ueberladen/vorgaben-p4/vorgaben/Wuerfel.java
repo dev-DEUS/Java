99debb06227fb4e1fb5c422d710edda6fc4a394d
dev-DEUS <70811837+dev-DEUS@users.noreply.github.com>
2024-10-06 15:29:47 +0200

    Aufgabe 4 vollstaendig bearbeitet.

--- Methoden (Klassen- und Instanzmethoden), Ueberladen/vorgaben-p4/vorgaben/Wuerfel.java
+++ Methoden (Klassen- und Instanzmethoden), Ueberladen/vorgaben-p4/vorgaben/Wuerfel.java
@@ -2,7 +2,19 @@ public class Wuerfel {
     public static int max = 6;  // maximale Augenzahl auf dem Wuerfel
     public int nr;
     public int wert;
+    public static int aktuelleNr = 1;
 
     /* *** Aufgabenteil (a) *** */
+    public Wuerfel() {
+        wert = 1;
+        nr = aktuelleNr++;
+    }
 
+    public void print() {
+        System.out.println("Wuerfel Nr. " + nr + " -> " + wert);
+    }
+
+    public void werfen() {
+        wert = (int) (Math.random() * max) + 1;
+    }
 }
--- Methoden (Klassen- und Instanzmethoden), Ueberladen/vorgaben-p4/vorgaben/WuerfelTest.java
+++ Methoden (Klassen- und Instanzmethoden), Ueberladen/vorgaben-p4/vorgaben/WuerfelTest.java
@@ -1,12 +1,23 @@
 public class WuerfelTest {
-    public static void main(String[] args){
-	int n = 3;
-	Wuerfel[] wuerfel = new Wuerfel[n];
+	public static void main(String[] args) {
+		int n = 3;
+		Wuerfel[] wuerfel = new Wuerfel[n];
 
-	/* *** Aufgabenteil (b) *** */
+		/* *** Aufgabenteil (b) *** */
 
-	System.out.println("---------------");
+		for (int i = 0; i < wuerfel.length; i++) {
+			Wuerfel myWuerfel = new Wuerfel(); // Erstellt bei jeder Iteration ein neues Objekt myWuerfel
+			wuerfel[i] = myWuerfel; // Fuellt das Feld wuerfel[] an der Stelle i mit dem erstellten Objekt
+			myWuerfel.print(); // Gibt die Wuerfel mit dem Startwert 1 aus
+		}
+		System.out.println("---------------");
 
-
-    }
+		for (int i = 0; i < wuerfel.length; i++) {
+			System.out.println((i + 1) + ". Wuerfeln:");
+			for (int j = 0; j < wuerfel.length; j++) {
+				wuerfel[j].werfen(); // Simuliert nun für jedes erstellte Objekt (Wuerfel) einen Wurf. 3 Wuerfe mit 3 Wuerfeln.
+				wuerfel[j].print(); // Gibt die simulierten Wuerfe an Stelle j aus.
+			}
+		}
+	}
 }
