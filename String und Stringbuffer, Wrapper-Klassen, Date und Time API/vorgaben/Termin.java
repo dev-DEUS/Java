603046073cfbfd57409c89aee3ce27e8ff353263
dev-DEUS <70811837+dev-DEUS@users.noreply.github.com>
2024-10-29 21:54:49 +0100

    Aufgabe 3 erfolgreich implementiert.

--- String und Stringbuffer, Wrapper-Klassen, Date und Time API/vorgaben/Termin.java
+++ String und Stringbuffer, Wrapper-Klassen, Date und Time API/vorgaben/Termin.java
@@ -3,6 +3,7 @@ import java.text.*;
 import java.time.*;
 import java.time.format.*;
 import java.time.temporal.*;
+import java.util.Locale;
 
 public class Termin {
     private LocalDateTime zeit; /*
@@ -22,10 +23,12 @@ public class Termin {
     }
 
     /* *** Aufgabenteil (a) *** */
-    public Termin(String datum, String uhrzeit, String beschreibung) {
+    public Termin(String datum, String uhrzeit, String beschreibung, int dauer) {
         DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
         LocalDateTime zeit = LocalDateTime.parse(datum + " " + uhrzeit, format);
         this.zeit = zeit;
+        this.dauer = Duration.ofMinutes(dauer);
+        this.todo = beschreibung;
     }
     /* *** Aufgabenteil (b) *** */
 
@@ -35,13 +38,59 @@ public class Termin {
                 zeit.getMinute());
         termin.dauer = Duration.ofMillis(dauer.toMillis());
         termin.todo = new String(todo);
+
         return termin; // Dummy Return, muss ersetzt werden
     }
 
     /* *** Aufgabenteil (c) *** */
+    public String toString() {
+        StringBuffer sbuf = new StringBuffer();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("EEEE, dd.MM.yyyy HH:mm").withLocale(Locale.GERMAN);
+
+        String beginn = format.format(zeit);
+        LocalDateTime endzeit = zeit.plus(dauer);
+        String ende = format.format(endzeit);
+
+        sbuf.append("Termin: ").append(todo).append("\n").append(String.format("Beginn: %s h", beginn)).append("\n")
+                .append(String.format("%-7s %s h", "Ende:", ende)).append("\n");
+
+        return sbuf.toString();
+    }
 
     /* *** Aufgabenteil (d) *** */
+    public static Termin[] serieGenerieren(Termin muster, int n, int k) {
+        Termin[] termine = new Termin[n];
+
+        for (int i = 0; i < n; i++) {
+            Termin kopTermin = (Termin) muster.clone();
+            kopTermin.zeit = kopTermin.zeit.plusDays(i * k);
+            termine[i] = kopTermin;
+        }
+        return termine;
+    }
 
     /* *** Aufgabenteil (e) *** */
+    public void verschieben(int anzahl, String einheit) {
+        ChronoUnit chronoUnit;
+
+        switch (einheit) {
+            case "Woche":
+                chronoUnit = ChronoUnit.WEEKS;
+                break;
+            case "Tag":
+                chronoUnit = ChronoUnit.DAYS;
+                break;
+            case "Stunde":
+                chronoUnit = ChronoUnit.HOURS;
+                break;
+            case "Minute":
+                chronoUnit = ChronoUnit.MINUTES;
+                break;
+            default:
+                return;
+        }
+
+        this.zeit = this.zeit.plus(anzahl, chronoUnit);
+    }
 
 }
--- String und Stringbuffer, Wrapper-Klassen, Date und Time API/vorgaben/Terminplaner.java
+++ String und Stringbuffer, Wrapper-Klassen, Date und Time API/vorgaben/Terminplaner.java
@@ -1,9 +1,29 @@
-/** Testklasse zum Testen der Klasse Termin 
+import java.time.Duration;
+
+/**
+ * Testklasse zum Testen der Klasse Termin
  */
-public class Terminplaner{
-    public static void main(String[] args){
-        
+public class Terminplaner {
+    public static void main(String[] args) {
+
         /* *** Aufgabenteil (f) *** */
+        Termin termin1 = new Termin("05.09.2024", "12:00", "Vorlesung PP1", 90);
+        Termin[] terminSerie = Termin.serieGenerieren(termin1, 14, 7);
+        for (int i = 0; i < terminSerie.length; i++) {
+            if (i == 4) {
+                terminSerie[i].verschieben(-1, "Tag");
+                System.out.println((i + 1) + ". " + terminSerie[i]);
+            } else {
+                System.out.println((i + 1) + ". " + terminSerie[i]);
+            }
+        }
+
+        Termin termin2 = new Termin("03.12.2024", "15:00", "Zahnarzt", 60);
+        System.out.println(termin2); // Termin vor der Verschiebung
+        termin2.verschieben(6, "Woche"); // Um Wochen verschieben
+        termin2.verschieben(-3, "Stunde"); // Um Stunden verschieben
+        termin2.verschieben(-30, "Minute"); // Um Minuten verschieben
+        System.out.println(termin2); // Nach der Verschiebung
 
     }
 }
