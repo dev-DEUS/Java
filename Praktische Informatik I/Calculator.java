8e7d1159b482e76bdfac47c830e6f44dff1f1ba7
dev-DEUS <70811837+dev-DEUS@users.noreply.github.com>
2024-11-13 21:30:30 +0100

    Sheet E und F erfolgreich implementiert.

--- Praktische Informatik I/Calculator.java
+++ Praktische Informatik I/Calculator.java
@@ -52,17 +52,71 @@ public class Calculator {
         return (i == 0) ? 0 : (i == 1) ? 1 : fib(i - 2) + fib(i - 1);
     }
 
+    public static int nthPrime(int n) {
+        int count = 0;
+        int num = 1;
+        while (count <= n) {
+            num++;
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+        return num;
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Add (int): " + add(24, 6));
-        System.out.println("Add (float): " + add(24f, 6f));
-        System.out.println("Sub (int): " + sub(24, 6));
-        System.out.println("Sub (float): " + sub(24f, 6f));
-        System.out.println("Mul (int): " + mul(24, 6));
-        System.out.println("Mul (float): " + mul(24f, 6f));
-        System.out.println("Div (int): " + div(24, 6));
-        System.out.println("Div (float): " + div(24f, 6f));
-        System.out.println(pow(2, 4));
-        System.out.println(fac(5));
-        System.out.println(fib(6));
+        /*
+         * System.out.println("Add (int): " + add(24, 6));
+         * System.out.println("Add (float): " + add(24f, 6f));
+         * System.out.println("Sub (int): " + sub(24, 6));
+         * System.out.println("Sub (float): " + sub(24f, 6f));
+         * System.out.println("Mul (int): " + mul(24, 6));
+         * System.out.println("Mul (float): " + mul(24f, 6f));
+         * System.out.println("Div (int): " + div(24, 6));
+         * System.out.println("Div (float): " + div(24f, 6f));
+         * System.out.println(pow(2, 4));
+         * System.out.println(fac(5));
+         * System.out.println(fib(6));
+         * 
+         */
+
+        int a = Integer.parseInt(args[0]);
+        String op = args[1];
+        int b = args.length > 2 ? Integer.parseInt(args[2]) : 0;
+
+        if (op.equals("+")) {
+            System.out.println(add(a, b));
+        } else if (op.equals("-")) {
+            System.out.println(sub(a, b));
+        } else if (op.equals("*")) {
+            System.out.println(mul(a, b));
+        } else if (op.equals("/")) {
+            System.out.println(div(a, b));
+        } else if (op.equals("%")) {
+            System.out.println(mod(a, b));
+        } else if (op.equals("^")) {
+            System.out.println(pow(a, b));
+        } else if (op.equals("!")) {
+            System.out.println(fac(a));
+        } else if (op.equals("fib")) {
+            System.out.println(fib(a));
+        } else if (op.equals("prime")) {
+            System.out.println(nthPrime(a));
+        } else {
+            System.out.println("Invalid operator");
+        }
+
     }
 }
