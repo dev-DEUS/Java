61537ce834c202538c36ce3b94b386e4bd467fd8
dev-DEUS <70811837+dev-DEUS@users.noreply.github.com>
2024-11-13 13:36:07 +0100

    Projekt 9 erfolgreich implementiert.

--- Programmierpraktikum I/Abstrakte Klassen und Interfaces/vorgaben/Bruch.java
+++ Programmierpraktikum I/Abstrakte Klassen und Interfaces/vorgaben/Bruch.java
@@ -1,15 +1,33 @@
-public class Bruch {
+public class Bruch implements Number {
     private int zaehler, nenner;
 
-    public Bruch(int z, int n){
+    public Bruch(int z, int n) {
         this.zaehler = z;
         this.nenner = n;
     }
 
-    public String toString(){
-        return "(" +  this.zaehler + "/" + this.nenner + ")";
+    public String toString() {
+        return "(" + this.zaehler + "/" + this.nenner + ")";
     }
 
     /* Aufgabenteil (c) */
+    public double toDouble() {
+        return (double) this.zaehler / this.nenner;
+    }
 
+    public boolean equals(Number number) {
+        if (number == null) {
+            return false;
+        }
+
+        return Double.compare(this.toDouble(), number.toDouble()) == 0;
+    }
+
+    public int compareTo(Number number) {
+        double value1 = this.toDouble();
+        double value2 = number.toDouble();
+
+        return Double.compare(value1, value2);
+
+    }
 }
--- Programmierpraktikum I/Abstrakte Klassen und Interfaces/vorgaben/TestNumbers.java
+++ Programmierpraktikum I/Abstrakte Klassen und Interfaces/vorgaben/TestNumbers.java
@@ -1,24 +1,46 @@
 public class TestNumbers {
 
     /* Aufgabenteil (a) */
-    public static void print(Number[] numbers){
- 
-    }
-    
-    /* Aufgabenteil (b) */ 
-    public static void sort(Number[] numbers){
- 
+    public static void print(Number[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i].toString() + " ");
+        }
     }
 
- 
+    /* Aufgabenteil (b) */
+    public static void sort(Number[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length - i; j++) {
+                if (numbers[j].compareTo(numbers[j + 1]) > 0) {
+                    Number temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int k = 10;
         Number[] brueche = new Number[k];
-        
+        int zaehler = 1, nenner = 0;
+
         /* Aufgabenteil (d) */
- 
+        for (int i = 0; i < brueche.length; i++) {
+            zaehler = (int) (Math.random() * 20) + 1; // Laut Aufgabenstellung sollen Zaehler und Nenner zufaellig
+                                                      // geneiert werden. Die Beispiel Ausgabe gibt jedoch als Zaehler
+                                                      // eine 1 vor. Falls dies gefordert ist, kann man einfach die
+                                                      // zufallsgeneration fuer den Zaehler auskommentieren und enthaelt
+                                                      // das geforderte Ergebnis.
+            nenner = (int) (Math.random() * 20) + 1;
+
+            Bruch bruch = new Bruch(zaehler, nenner);
+            brueche[i] = bruch;
+        }
+
         print(brueche);
         sort(brueche);
+        System.out.println();
         print(brueche);
     }
 }
